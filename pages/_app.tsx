f2f478d62d5de84cc801d8fbc0a20a4e4619020d
devk <devk@umich.edu>
2022-12-13 03:56:40 -0500

    Fixed all merge errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Bio from "./bio/bio";
+import Courses from "./courses";
+import { Fragment } from "react";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Bio />
+      <Courses />
+    </Fragment>
+  );
 }
