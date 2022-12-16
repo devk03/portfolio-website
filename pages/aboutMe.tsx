133c359fda30a4e8efb7ddb74b32648fac8d5a03
devk <devk@umich.edu>
2022-12-16 11:38:00 -0500

    Added more text to the about me section

--- pages/aboutMe.tsx
+++ pages/aboutMe.tsx
@@ -4,7 +4,10 @@ const AboutMe = () => {
     return (
         <div className={styles.box}>
             <h2 className={styles.aboutMe}>About Me</h2>
-            <p className={styles.p}>Hi! My name is Dev Kunjadia. I am a student at the University of Michigan studying Computer Science and Mathematics. I am seeking roles in software engineering, quantitative trading, and machine learning.</p>
+            <p className={styles.p}>Hi! My name is Dev Kunjadia. I am a student at the University of Michigan 
+            studying Computer Science and Mathematics. I am seeking roles in software engineering, 
+            quantitative trading, and machine learning. For fun I like to play chess, play poker, weight train, read, wrestle, and listen to music! 
+            Feel free to email me at devk@umich.edu for any proffesional inquiries.</p>
         </div>
     )
 }
