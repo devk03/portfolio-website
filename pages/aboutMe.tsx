777aa18b5c6199d178a423a00942a221538746c8
devk <devk@umich.edu>
2022-12-16 11:39:54 -0500

    Fixed typo in aboutMe section

--- pages/aboutMe.tsx
+++ pages/aboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
             <p className={styles.p}>Hi! My name is Dev Kunjadia. I am a student at the University of Michigan 
             studying Computer Science and Mathematics. I am seeking roles in software engineering, 
             quantitative trading, and machine learning. For fun I like to play chess, play poker, weight train, read, wrestle, and listen to music! 
-            Feel free to email me at devk@umich.edu for any proffesional inquiries.</p>
+            Feel free to email me at devk@umich.edu for any professional inquiries.</p>
         </div>
     )
 }
