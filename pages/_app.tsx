2df712054ec47e4650fda282fa5e0cebc61cddcb
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-05-27 02:46:12 -0400

    Removed unnecessary stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Courses from "./homepage/courses";
 import { Fragment } from "react";
 import AboutMe from "./homepage/aboutMe";
 import Bio from "./homepage/bio";
-import Blog from "./homepage/blog";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -15,14 +13,14 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <header className="z-10 text-xl md:text-2xl py-4 bg-zinc-900 text-center text-white sticky top-0">
-        <nav className="flex flex-wrap justify-center px-2">
+        <div className="flex flex-wrap justify-center px-2">
           <a href="#contact" className="px-2 py-1">
             Contact
           </a>
           <a href="#bio" className="px-2 py-1">
             About Me
           </a>
-        </nav>
+        </div>
       </header>
       <div className="snap-y snap-mandatory h-screen min-w-full overflow-auto">
         <div className="snap-start flex items-center justify-center min-h-screen min-w-full bg-sky-100">
--- pages/homepage/aboutMe.tsx
+++ pages/homepage/aboutMe.tsx
@@ -4,16 +4,16 @@ import Image from "next/image";
 const AboutMe = () => {
   return (
     <div className="px-4">
-      <p className="text-6xl sm:text-8xl lg:text-9xl text-center font-extrabold py-4">
+      <div className="text-6xl sm:text-8xl lg:text-9xl text-center font-extrabold py-4">
         Dev Kunjadia
-      </p>
-      <p className="font-bold text-2xl sm:text-4xl lg:text-5xl text-center py-4">
+      </div>
+      <div className="font-bold text-2xl sm:text-4xl lg:text-5xl text-center py-4">
         Logician. Athlete. Builder. Artist.
-      </p>
-      <p className="font-semibold text-xl sm:text-2xl text-center py-4">
+      </div>
+      <div className="font-semibold text-xl sm:text-2xl text-center py-4">
         Student @ The University of Michigan <br /> Software Engineering
         Intern @ SkySpecs
-      </p>
+      </div>
       <div className="flex flex-wrap justify-center items-center pt-10 space-x-4">
         <a href="Kunjadia_resume.pdf" download>
           <Image
