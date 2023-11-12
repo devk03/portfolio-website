3f21c4a5e714029ae84994ceab1b9e1436557334
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-11-12 01:17:01 -0500

    got blog to render html markdown using dom purify, defined post typescript interface

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import { useEffect } from 'react'
-import BlogForm from '../../components/blogForm'
-import {db} from '../../firebase';
-const Blog = () => {
-  return (
-    <div>
-      Load posts here
-    </div>
-  )
+import React, { useEffect, useState } from 'react';
+import { db } from '../../firebase';
+import { collection, getDocs, DocumentData } from 'firebase/firestore';
+import DOMPurify from 'dompurify';
+
+// Define the structure of a post object
+interface Post {
+  id: string;
+  content: string;
+  header: string;
+  // include other fields you expect to have
 }
 
-export default Blog
+const Blog = () => {
+  const [posts, setPosts] = useState<Post[]>([]); // Use the Post interface
+
+  useEffect(() => {
+    const colRef = collection(db, 'posts');
+
+    getDocs(colRef)
+      .then((snapshot) => {
+        const postsData: Post[] = []; // Use the Post interface
+        snapshot.forEach((doc: DocumentData) => {
+          const postData = { id: doc.id, ...doc.data() } as Post; // Cast to the Post type
+          postsData.push(postData);
+        });
+        setPosts(postsData);
+      })
+      .catch((error) => {
+        console.error('Error getting documents:', error);
+      });
+  }, []);
+
+  // Function to sanitize and create markup for dangerouslySetInnerHTML
+  const createMarkup = (htmlContent: string) => {
+    return { __html: DOMPurify.sanitize(htmlContent) };
+  };
+
+  return (
+    <div>
+      {posts.map((post: Post) => (
+        <div key={post.id}>
+          <h2>{post.header}</h2>
+          <div dangerouslySetInnerHTML={createMarkup(post.content)} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Blog;
